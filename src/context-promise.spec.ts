c266dd73ebeb88752ebb52bf4cd44651736a66e7
Austin Whittier <austin.whitt@gmail.com>
2017-08-25 10:27:28 -0600

    add thenBag and remove thenBasic

--- src/context-promise.spec.ts
+++ src/context-promise.spec.ts
@@ -1,35 +1,96 @@
-import { ContextPromise } from "./context-promise";
+import { ContextPromise } from './context-promise';
 
-it("ContextPromise is defined", () => {
+it('ContextPromise is defined', () => {
   expect(ContextPromise).toBeTruthy();
 });
 
-it("Basic promise", () => {
-  const p = new ContextPromise(Promise.resolve({ greeting: "howdy" }));
-  return expect(p).resolves.toEqual({ greeting: "howdy" });
+describe('Basic', () => {
+  it('Basic promise', () => {
+    const p = new ContextPromise(Promise.resolve({ greeting: 'howdy' }));
+    return expect(p).resolves.toEqual({ greeting: 'howdy' });
+  });
+
+  it('Basic chaining', () => {
+    const p = new ContextPromise(
+      Promise.resolve({ greeting: 'howdy' }),
+    ).then(c => ({ greets: c.greeting }));
+
+    return expect(p).resolves.toEqual({ greeting: 'howdy', greets: 'howdy' });
+  });
+
+  it('Raining example', () => {
+    const promptZip = () => Promise.resolve('94107');
+    const getTemp = (zip: string) => Promise.resolve(50);
+    const getRain = (zip: string) => Promise.resolve(true);
+    const shouldWearJacket = (temp: number, raining: boolean) => {
+      return temp <= 50 || raining;
+    };
+
+    const p = new ContextPromise(promptZip().then(zip => ({ zip })))
+      .then(c => getTemp(c.zip).then(temp => ({ temp })))
+      .then(c => getRain(c.zip).then(rain => ({ rain })))
+      .then(c => ({ should: shouldWearJacket(c.temp, c.rain) }));
+
+    return expect(p).resolves.toHaveProperty('should');
+  });
 });
 
-it("Basic chaining", () => {
-  const p = new ContextPromise(
-    Promise.resolve({ greeting: "howdy" })
-  ).then(c => ({ greets: c.greeting }));
+describe('Named input', () => {
+  it('Basic chaining', () => {
+    const p = new ContextPromise(Promise.resolve({ greeting: 'howdy' })).then(
+      'greets',
+      c => c.greeting,
+    );
 
-  return expect(p).resolves.toEqual({ greeting: "howdy", greets: "howdy" });
+    return expect(p).resolves.toEqual({ greeting: 'howdy', greets: 'howdy' });
+  });
 });
 
-it("Raining example", () => {
-  const promptZip = () => Promise.resolve("94107");
-  const getTemp = (zip: string) => Promise.resolve(50);
-  const getRain = (zip: string) => Promise.resolve(true);
-  const shouldWearJacket = (temp: number, raining: boolean) => {
-    return temp <= 50 || raining;
-  };
+describe('Bag', () => {
+  it('Weather example', () => {
+    const promptZip = () => Promise.resolve('94107');
+    const getTemp = (zip: string) => {
+      return new Promise<number>((resolve, reject) => {
+        setTimeout(() => resolve(50), 10);
+      });
+    };
+    const getRain = (zip: string) => Promise.resolve(true);
+    const shouldWearJacket = (temp: number, raining: boolean) => {
+      return temp <= 50 && raining;
+    };
 
-  const p = new ContextPromise(promptZip().then(zip => ({ zip })))
-    .then(c => getTemp(c.zip).then(temp => ({ temp })))
-    .then(c => getRain(c.zip).then(rain => ({ rain })))
-    .then(c => ({ should: shouldWearJacket(c.temp, c.rain) }));
+    const p = new ContextPromise(promptZip().then(zip => ({ zip })))
+      .then(c => ({
+        temp: getTemp(c.zip),
+        rain: getRain(c.zip),
+      }))
+      .then(c => ({
+        should: shouldWearJacket(c.temp, c.rain),
+      }));
 
-  return expect(p).resolves.toHaveProperty("should");
+    return expect(p).resolves.toEqual({
+      zip: '94107',
+      temp: 50,
+      rain: true,
+      should: true,
+    });
+  });
+
+  it('Slightly more complex example', () => {
+    const getTemp = (zip: string) => {
+      return new Promise<{ temperature: number }>((resolve, reject) => {
+        setTimeout(() => resolve({ temperature: 50 }), 100);
+      });
+    };
+
+    const p = new ContextPromise(
+      getTemp('94107').then(temp => ({ tempResponse: temp })),
+    );
+
+    return expect(p).resolves.toEqual({
+      tempResponse: {
+        temperature: 50,
+      },
+    });
+  });
 });
-
--- src/context-promise.ts
+++ src/context-promise.ts
@@ -1,3 +1,10 @@
+function isPojo(obj: any): obj is { [k: string]: any } {
+  if (obj === null || typeof obj !== 'object') {
+    return false;
+  }
+  return Object.getPrototypeOf(obj) === Object.prototype;
+}
+
 class ContextPromise<C = {}, U = {}> implements PromiseLike<C & U> {
   contextProvider: PromiseLike<C>;
 
@@ -5,21 +12,75 @@ class ContextPromise<C = {}, U = {}> implements PromiseLike<C & U> {
     this.contextProvider = Promise.resolve(value);
   }
 
+  /** Bag */
   then<TResult1 = U, TResult2 = never>(
-    onfulfilled?: ((value: C) => TResult1 | PromiseLike<TResult1>)
-    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>)
+    onfulfilled?: (
+      value: C,
+    ) => { [k in keyof TResult1]: PromiseLike<TResult1[k]> | TResult1[k] },
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>),
+  ): ContextPromise<C & TResult1>;
+
+  /** Named */
+  then<K extends string, TResult1 = U, TResult2 = never>(
+    key: K,
+    onfulfilled?: ((value: C) => TResult1 | PromiseLike<TResult1>),
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>),
+  ): ContextPromise<C & { [k in K]: TResult1 }>;
+
+  then(A: any, B: any, C: any): any {
+    if (typeof A === 'string') {
+      return this.thenNamed(A, B, C);
+    } else {
+      return this.thenBag(A, B);
+    }
+  }
+
+  private thenBag<TResult1 = U, TResult2 = never>(
+    onfulfilled?: (
+      value: C,
+    ) => { [k in keyof TResult1]: PromiseLike<TResult1[k]> | TResult1[k] },
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>),
   ): ContextPromise<C & TResult1> {
-    return new ContextPromise(new Promise((resolve, reject) => {
-      this.contextProvider
-        .then((c: C) => {
-          Promise.resolve(onfulfilled(c))
-            .then((u: TResult1) => {
-              resolve(Object.assign({}, c, u));
-            })
-            .catch(reject);
-        })
-        .catch(reject);
-    }))
+    return new ContextPromise(
+      new Promise((resolve, reject) => {
+        this.contextProvider.then((c: C) => {
+          const promises = onfulfilled(c);
+          if (isPojo(promises)) {
+            const transformed = Object.keys(promises).map(k => {
+              return Promise.resolve(promises[k]).then(tk => ({ [k]: tk }));
+            });
+
+            Promise.all(transformed)
+              .then(values => {
+                resolve(Object.assign.apply(null, [{}, c, ...values]));
+              })
+              .catch(reject);
+          } else {
+            resolve(Object.assign({}, c));
+          }
+        });
+      }),
+    );
+  }
+
+  private thenNamed<TResult1 = U, TResult2 = never, K extends string>(
+    key: K,
+    onfulfilled?: ((value: C) => TResult1 | PromiseLike<TResult1>),
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>),
+  ): ContextPromise<C & { [k in K]: TResult1 }> {
+    return new ContextPromise(
+      new Promise((resolve, reject) => {
+        this.contextProvider
+          .then((c: C) => {
+            Promise.resolve(onfulfilled(c))
+              .then((u: TResult1) => {
+                resolve(Object.assign({}, c, { [key]: u }));
+              })
+              .catch(reject);
+          })
+          .catch(reject);
+      }),
+    );
   }
 }
 
