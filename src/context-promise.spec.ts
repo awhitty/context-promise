afc527e1397c5a91ae3b15a061688a5677756f30
Austin Whittier <austin.whitt@gmail.com>
2017-09-06 23:54:10 -0700

    replace tests with more deliberate ones

--- src/context-promise.spec.ts
+++ src/context-promise.spec.ts
@@ -1,195 +1,263 @@
 import { ContextPromise } from './context-promise';
 
-it('ContextPromise is defined', () => {
-  expect(ContextPromise).toBeTruthy();
-});
-
-describe('Basic', () => {
-  it('Basic promise', () => {
-    const p = ContextPromise.fromPromise(
-      Promise.resolve({ greeting: 'howdy' }),
-    );
-    return expect(p).resolves.toEqual({ greeting: 'howdy' });
+describe('ContextPromise', () => {
+  it('ContextPromise is defined', () => {
+    expect(ContextPromise).toBeTruthy();
   });
 
-  it('Basic chaining', () => {
-    const p = ContextPromise.fromPromise(
-      Promise.resolve({ greeting: 'howdy' }),
-    ).then(c => ({ greets: c.greeting }));
-
-    return expect(p).resolves.toEqual({ greeting: 'howdy', greets: 'howdy' });
-  });
-
-  it('Raining example', () => {
-    const promptZip = () => Promise.resolve('94107');
-    const getTemp = (zip: string) => Promise.resolve(50);
-    const getRain = (zip: string) => Promise.resolve(true);
-    const shouldWearJacket = (temp: number, raining: boolean) => {
-      return temp <= 50 || raining;
-    };
-
-    const p = ContextPromise.fromPromise(promptZip().then(zip => ({ zip })))
-      .then(c => getTemp(c.zip).then(temp => ({ temp })))
-      .then(c => getRain(c.zip).then(rain => ({ rain })))
-      .then(c => ({ should: shouldWearJacket(c.temp, c.rain) }));
-
-    return expect(p).resolves.toEqual({
-      zip: '94107',
-      temp: 50,
-      rain: true,
-      should: true,
-    });
-  });
-});
-
-describe('Named input', () => {
-  it('Basic chaining', () => {
-    const p = ContextPromise.fromPromise(Promise.resolve({ greeting: 'howdy' }))
-      .then('greets', c => c.greeting)
-      .then(c => console.log);
-
-    return expect(p).resolves.toEqual({ greeting: 'howdy', greets: 'howdy' });
-  });
-});
-
-describe('Bag', () => {
-  it('Weather example', () => {
-    const promptZip = () => Promise.resolve('94107');
-    const getTemp = (zip: string) => {
-      return new Promise<number>((resolve, reject) => {
-        setTimeout(() => resolve(50), 10);
+  describe('constructor', () => {
+    it('constructs a ContextPromise object', () => {
+      const cp = new ContextPromise((resolve, reject) => {
+        resolve({ greeting: 'Hi' });
       });
-    };
-    const getRain = (zip: string) => Promise.resolve(true);
-    const shouldWearJacket = (temp: number, raining: boolean) => {
-      return temp <= 50 && raining;
-    };
 
-    const p = ContextPromise.fromPromise(promptZip().then(zip => ({ zip })))
-      .then(c => ({
-        temp: getTemp(c.zip),
-        rain: getRain(c.zip),
-      }))
-      .then(c => ({
-        should: shouldWearJacket(c.temp, c.rain),
-      }));
-
-    return expect(p).resolves.toEqual({
-      zip: '94107',
-      temp: 50,
-      rain: true,
-      should: true,
+      expect(cp).toBeInstanceOf(ContextPromise);
     });
-  });
 
-  it('Slightly more complex example', () => {
-    const getTemp = (zip: string) => {
-      return new Promise<{ temperature: number }>((resolve, reject) => {
-        setTimeout(() => resolve({ temperature: 50 }), 100);
+    it('rejects properly', () => {
+      const cp = new ContextPromise((resolve, reject) => {
+        throw Error;
       });
-    };
 
-    const p = ContextPromise.fromPromise(
-      getTemp('94107').then(temp => ({ tempResponse: temp })),
-    );
-
-    return expect(p).resolves.toEqual({
-      tempResponse: {
-        temperature: 50,
-      },
-    });
-  });
-});
-
-describe('Tap', () => {
-  it('should allow tapping into context', done => {
-    ContextPromise.fromPromise(Promise.resolve({ phrase: 'yolo' })).tap(c => {
-      expect(c).toEqual({ phrase: 'yolo' });
-      done();
+      expect(cp).rejects.toBe(Error);
     });
   });
 
-  it('should handle error after tap', done => {
-    const spy = jest.fn();
-    ContextPromise.fromPromise(Promise.resolve({ phrase: 'yolo' }))
-      .tap(c => {
-        throw new Error('Arbitrary error');
-      })
-      .then(spy, e => {
-        expect(spy.mock.calls.length).toEqual(0);
-        expect(e).toEqual(new Error('Arbitrary error'));
-        done();
-      });
-  });
-});
-
-describe('Error handling', () => {
-  it('should handle rejection from constructor', () => {
-    const p = new ContextPromise((resolve, reject) => {
-      reject(new Error('Arbitrary error'));
-    });
-
-    return expect(p).rejects.toEqual(new Error('Arbitrary error'));
-  });
-
-  it('should reject as second parameter to then', done => {
-    ContextPromise.fromPromise(Promise.resolve({ phrase: 'yolo' }))
-      .then(c => {
-        throw new Error('Arbitrary error');
-      })
-      .then(
-        c => ({}),
-        e => {
-          expect(e).toEqual(new Error('Arbitrary error'));
-          done();
-        },
+  describe('fromPromise', () => {
+    it('constructs a ContextPromise object', () => {
+      const cp = ContextPromise.fromPromise(
+        Promise.resolve({ greeting: 'Hi' }),
       );
-  });
-
-  it('should handle rejection after then', () => {
-    const p = ContextPromise.fromPromise(
-      Promise.resolve({ phrase: 'yolo' }),
-    ).then(c => {
-      throw new Error('Arbitrary error');
+      expect(cp).toBeInstanceOf(ContextPromise);
     });
 
-    return expect(p).rejects.toEqual(new Error('Arbitrary error'));
-  });
-
-  it('should handle rejection with keyed then', () => {
-    const p = ContextPromise.fromPromise(
-      Promise.resolve({ phrase: 'yolo' }),
-    ).then('someKey', c => {
-      throw new Error('Arbitrary error');
+    it('resolves to the value of the promise', () => {
+      const cp = ContextPromise.fromPromise(
+        Promise.resolve({ greeting: 'Hi' }),
+      );
+      expect(cp).resolves.toEqual({ greeting: 'Hi' });
     });
-
-    return expect(p).rejects.toEqual(new Error('Arbitrary error'));
   });
 
-  it('should handle rejection inside PromiseBag', () => {
-    const p = ContextPromise.fromPromise(
-      Promise.resolve({ phrase: 'yolo' }),
-    ).then(c => ({
-      noError: 'pleasantly fine',
-      someError: Promise.reject(new Error('Arbitrary error')),
-      noOtherError: 'chillin here',
-    }));
-
-    return expect(p).rejects.toEqual(new Error('Arbitrary error'));
-  });
-
-  it('should not fire subsequent thens', done => {
-    const spy = jest.fn();
-
-    ContextPromise.fromPromise(Promise.resolve({ phrase: 'yolo' }))
-      .then(c => ({
-        noError: 'pleasantly fine',
-        someError: Promise.reject(new Error('Arbitrary error')),
-        noOtherError: 'chillin here',
-      }))
-      .then(spy as any, () => {
-        expect(spy.mock.calls.length).toEqual(0);
-        done();
+  describe('fromBag', () => {
+    it('constructs a ContextPromise object', () => {
+      const cp = ContextPromise.fromBag({
+        greeting: Promise.resolve('hi'),
       });
+
+      expect(cp).toBeInstanceOf(ContextPromise);
+    });
+
+    it('resolves the contents of the bag', () => {
+      const cp = ContextPromise.fromBag({
+        greeting: Promise.resolve('hi'),
+        name: 'Gert',
+      });
+
+      expect(cp).resolves.toEqual({
+        greeting: 'hi',
+        name: 'Gert',
+      });
+    });
+  });
+
+  describe('then', () => {
+    describe('Unnamed', () => {
+      it('provides context to chain', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        }).then(c => {
+          expect(c).toEqual({ name: 'Gert' });
+          done();
+        });
+      });
+
+      it('provides context to double chain', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => {
+            return {
+              greeting: `Hi, ${c.name}`,
+            };
+          })
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('resolves promises', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => Promise.resolve({ greeting: `Hi, ${c.name}` }))
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('resolves promises in a bag', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => {
+            return {
+              greeting: Promise.resolve(`Hi, ${c.name}`),
+            };
+          })
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('rejects when error in fulfilled handler', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => {
+            throw Error;
+          })
+          .then(
+            c => {},
+            e => {
+              expect(e).toBe(Error);
+              done();
+            },
+          );
+      });
+
+      it('rejects when sub-promise rejects', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => Promise.reject(Error))
+          .then(
+            c => {},
+            e => {
+              expect(e).toBe(Error);
+              done();
+            },
+          );
+      });
+
+      it('rejects when sub-promise in bag rejects', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => {
+            return {
+              greeting: Promise.reject(Error),
+            };
+          })
+          .then(
+            c => {},
+            e => {
+              expect(e).toBe(Error);
+              done();
+            },
+          );
+      });
+    });
+
+    describe('Named', () => {
+      it('accepts named payload', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then('greeting', c => `Hi, ${c.name}`)
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('resolves promises from named payload', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then('greeting', c => Promise.resolve(`Hi, ${c.name}`))
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('resolves context even when intermediate returns undefined', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then(c => {
+            return;
+          })
+          .then('greeting', c => `Hi, ${c.name}`)
+          .then(c => {
+            expect(c).toEqual({
+              name: 'Gert',
+              greeting: 'Hi, Gert',
+            });
+            done();
+          });
+      });
+
+      it('rejects when error in named fulfilled handler', done => {
+        ContextPromise.fromBag({
+          name: Promise.resolve('Gert'),
+        })
+          .then('greeting', c => {
+            throw Error;
+          })
+          .then(
+            c => {},
+            e => {
+              expect(e).toBe(Error);
+              done();
+            },
+          );
+      });
+    });
+  });
+
+  describe('toPromise', () => {
+    it('returns a promise', () => {
+      const promise = ContextPromise.fromBag({
+        name: Promise.resolve('Gert'),
+      }).toPromise();
+
+      expect(promise).toBeInstanceOf(Promise);
+      expect(promise).not.toBeInstanceOf(ContextPromise);
+    });
+
+    it('resolves to the original context', done => {
+      ContextPromise.fromBag({
+        name: Promise.resolve('Gert'),
+      })
+        .then('greeting', c => `Hi, ${c.name}`)
+        .toPromise()
+        .then(c => {
+          expect(c).toEqual({
+            name: 'Gert',
+            greeting: 'Hi, Gert',
+          });
+          done();
+        });
+    });
   });
 });
